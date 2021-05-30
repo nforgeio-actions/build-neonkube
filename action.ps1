9e403c52351b0af0ceabf09458d35bdf762b283b
Jeff Lill <jeff@lilltek.com>
2021-05-29 20:56:35 -0700

    scripted push

--- action.ps1
+++ action.ps1
@@ -104,7 +104,7 @@ try
 
     # Perform the build.
 
-    pwsh -File $buildScript -NonInteractive $configOption $toolsOption $installersOption $codeDocOption 6>&1 2>&1 > $buildLogPath
+    pwsh -File $buildScript -NonInteractive $configOption $toolsOption $installersOption $codeDocOption 2>&1 > $buildLogPath
     ThrowOnExitCode
 }
 catch
